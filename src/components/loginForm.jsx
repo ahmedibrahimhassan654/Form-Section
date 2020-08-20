e304b8142f9f6299a3de9173043a34105f86d9d0
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-08-21 00:18:28 +0200

    lec 3 handling form submission

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,26 +1,23 @@
 //imrc
 import React, { Component } from 'react'
 class LoginForm extends Component {
+  handleSubmet = (e) => {
+    e.preventDefault()
+
+    console.log('submeted')
+  }
+
   render() {
     return (
       <div className='container'>
-        <form>
+        <form onSubmit={this.handleSubmet}>
           <div className='form-group'>
             <label htmlFor='username'>Email address</label>
-            <input
-              type='text'
-              className='form-control'
-              id='username'
-             
-            />
+            <input type='text' className='form-control' id='username' />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-            />
+            <input type='password' className='form-control' id='password' />
           </div>
 
           <button type='submit' className='btn btn-primary'>
